24a202e0dc954927353094105f06ef71fc499d07
ILIA0 <155063101+ILIA090@users.noreply.github.com>
2025-10-22 22:11:11 +0330

    Update Hamyarsibery.py

--- Hamyarsibery.py
+++ Hamyarsibery.py
@@ -42,3 +42,7 @@ def receive_update():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
+@app.route("/")
+def home():
+    return "🤖 Rubika AI bot by ilia manzari is running!"
